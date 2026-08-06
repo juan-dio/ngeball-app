b5e428ef14bccd7c8cface8368170b9701360623
juan-dio <juanaxl432@gmail.com>
2026-08-06 13:26:19 +0700

    style: standardize eg placeholder in email inputs

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -137,7 +137,7 @@ export default function RegisterPage() {
                   <Input
                     id="email"
                     type="email"
-                    placeholder="eg, youremail@gmail.com"
+                    placeholder="eg. youremail@gmail.com"
                     value={email}
                     onChange={(e) => {
                       setEmail(e.target.value);
